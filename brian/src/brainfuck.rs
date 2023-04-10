ad21e68a1699be6c43e67d5ba471667fbf637b02
griffi-gh <prasol258@gmail.com>
2023-04-11 00:34:58 +0200

    update

--- brian/src/brainfuck.rs
+++ brian/src/brainfuck.rs
@@ -26,7 +26,7 @@ pub enum Token {
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum Opcode {
   IncrementRelative(isize, isize),
-  SetRelative(isize, usize),
+  SetRelative(isize, u8),
   MovePointer(isize),
   LoopStart,
   LoopEnd,
@@ -126,31 +126,77 @@ impl Brainfuck {
 
   fn optimize(ops: Vec<Opcode>) -> Vec<Opcode> {
     let mut output_ops: Vec<Opcode> = Vec::new();
-    let mut ptr_offset: isize = 0;
-    let mut block_increments: HashMap<isize, isize> = HashMap::new();
-    //Optimize increments/ptr movements
-    for op in ops {
-      match op {
-        Opcode::IncrementRelative(offset, increment) => {
-          let existing_value = *block_increments.get(&ptr_offset).unwrap_or(&0);
-          block_increments.insert(offset + ptr_offset, existing_value + increment);
-        }
-        Opcode::MovePointer(diff) => {
-          ptr_offset += diff;
-        },
-        Opcode::Eof | Opcode::Input | Opcode::Output | Opcode::LoopStart | Opcode::LoopEnd => {
-          //commit increments and pointer movements
-          for (relative_pos, increment) in block_increments.iter().sorted_by_key(|x| *x.0) {
-            if *increment == 0 { continue }
-            output_ops.push(Opcode::IncrementRelative(*relative_pos, *increment))
+    {
+      #[derive(Clone, Copy, PartialEq, Eq)]
+      enum BlockEffect {
+        Increment(isize),
+        Set(u8)
+      }
+      let mut ptr_offset: isize = 0;
+      let mut block_effects: HashMap<isize, BlockEffect> = HashMap::new();
+      //TODO: unlink loops
+      //TODO: check for eof token and add it
+      //TODO: recursive block compilation
+      //Optimize increments/ptr movements
+      let mut index = 0;
+      while index < ops.len() {
+        let op = &ops[index];
+        index += 1;
+        match op {
+          Opcode::IncrementRelative(offset, increment) => {
+            let existing_effect = block_effects.get_mut(&ptr_offset);
+            match existing_effect {
+              Some(BlockEffect::Increment(effect)) => {
+                *effect += increment;
+              },
+              Some(BlockEffect::Set(effect)) => {
+                //TODO casting isize to i8 can cause unexpected behaviour here!
+                *effect = effect.wrapping_add_signed(*increment as i8);
+              },
+              None => {
+                block_effects.insert(offset + ptr_offset, BlockEffect::Increment(*increment));
+              }
+            }
+            //block_effects.insert(offset + ptr_offset, existing_value + increment);
           }
-          output_ops.push(Opcode::MovePointer(ptr_offset));
-          output_ops.push(op);
-          block_increments.clear();
-        },
-        _ => todo!()
+          Opcode::MovePointer(diff) => {
+            ptr_offset += diff;
+          },
+          Opcode::Eof | Opcode::Input | Opcode::Output | Opcode::LoopStart | Opcode::LoopEnd => {
+            //Detect [-]/[+] loops (TODO compute block effects instead!)
+            if let Opcode::LoopStart = op {
+              if let Opcode::LoopEnd = ops[index + 1] {
+                if let Opcode::IncrementRelative(pos, value) = ops[index] {
+                  if pos == 0 && value.abs() % 2 == 1 {
+                    index += 2;
+                    block_effects.insert(ptr_offset, BlockEffect::Set(0));
+                    continue
+                  }
+                }
+              }
+            }
+            //commit increments and pointer movements
+            for (relative_pos, effect) in block_effects.iter().sorted_by_key(|x| *x.0) {
+              output_ops.push(match effect {
+                BlockEffect::Increment(increment) => {
+                  if *increment == 0 { continue }
+                  Opcode::IncrementRelative(*relative_pos, *increment)
+                },
+                BlockEffect::Set(value) => Opcode::SetRelative(*relative_pos, *value),
+              })
+            }
+            if ptr_offset > 0 {
+              output_ops.push(Opcode::MovePointer(ptr_offset));
+            }
+            //TODO: figure out a way to avoid clone (low prio; opt times arent that important)
+            output_ops.push(op.clone()); 
+            block_effects.clear();
+          },
+          _ => todo!()
+        }
       }
     }
+
     //Link loops
     {
       let mut stack: Vec<usize> = Vec::new();
@@ -172,6 +218,7 @@ impl Brainfuck {
       }
       assert!(stack.is_empty(), "Unclosed loop");
     }
+
     output_ops
   }
 
