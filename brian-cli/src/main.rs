737e233efbd3591bcc34004f68ed79f9d6ec4e32
griffi-gh <prasol258@gmail.com>
2023-04-11 03:44:27 +0200

    add time logging

--- brian-cli/src/main.rs
+++ brian-cli/src/main.rs
@@ -1,8 +1,12 @@
 use brian::Brainfuck;
+use std::time::Instant;
 
 fn main() {
   let mut bf = Brainfuck::new();
   bf.compile(include_str!("../../malderbrot.b.txt"));
   bf._debug();
+  let start = Instant::now();
   bf.run();
+  let elapsed_ms = start.elapsed().as_secs_f64();
+  println!("Took {} seconds", elapsed_ms);
 }
